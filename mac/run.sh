e27711f60bf47f8d27445c2070f20ef351d5502b
Christian Wahl <christian@cwahl.eu>
2021-03-29 12:54:29 +0200

    fix missing directory in mac/run.sh

--- mac/run.sh
+++ mac/run.sh
@@ -13,7 +13,7 @@ QEMU_PID=$!
 echo "QEMU runs with PID $QEMU_PID"
 read -n 1 -s -r -p "Press any key to continue"
 
-arm-none-eabi-gdb -tui --symbols=$NAME -iex "target remote :1234" -iex "layout regs" -ex "b stop"
+arm-none-eabi-gdb -tui --symbols="$OUTPUT/$NAME" -iex "target remote :1234" -iex "layout regs" -ex "b stop"
 
 kill -9 $QEMU_PID
 

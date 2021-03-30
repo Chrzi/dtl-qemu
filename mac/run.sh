2d66d33ae399412947dd33582742e9c5b5de9cb2
Christian Wahl <christian@cwahl.eu>
2021-03-30 18:57:52 +0200

    Update run.sh

--- mac/run.sh
+++ mac/run.sh
@@ -1,13 +1,14 @@
 #!/usr/bin/env bash
 CPU="arm7tdmi-s"
 QEMU_CPU="arm926"
+QEMU_MACHINE="realview-eb"
 ENTRY="_startup"
 NAME="$(basename $1 .S)"
 OUTPUT="$(dirname "$0")/build"
 
 ./assemble.sh $1
 
-qemu-system-arm -s -S -machine virt -kernel "$OUTPUT/$NAME" -nographic &
+qemu-system-arm -s -S -machine $QEMU_MACHINE -cpu $QEMU_CPU -kernel "$OUTPUT/$NAME" -nographic &
 QEMU_PID=$!
 
 echo "QEMU runs with PID $QEMU_PID"
